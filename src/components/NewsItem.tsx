7997cac8453170802981f67a5fdd46b45d936385
BrianSorensen <contact@fiduzen.dk>
2025-06-22 17:32:43 +0200

    List, item and expanding done

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -1,9 +1,72 @@
 import { useEffect, useState } from "react";
 
-const NewsItem = ({ id }: { id: String }) => {
-  useEffect(() => {}, []);
+interface Story {
+  id: string;
+  deleted: boolean;
+  type: string;
+  by: string;
+  time: number;
+  text: string;
+  dead: boolean;
+  parent: string;
+  poll: string;
+  kids: string[];
+  url: string;
+  score: number;
+  title: string;
+  parts: string[];
+  descendants: number;
+}
 
-  return <p>news Item</p>;
+interface Author {
+  id: string;
+  created: number;
+  karma: number;
+  about: string;
+  submitted: string[];
+}
+
+const NewsItem = ({ id }: { id: String }) => {
+  const [story, setStory] = useState<Story>();
+  const [author, setAuthor] = useState<Author>();
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  useEffect(() => {
+    fetch("https://hacker-news.firebaseio.com/v0/item/" + id + ".json")
+      .then((response) => response.json())
+      .then((data) => setStory(data));
+  }, []);
+
+  const handleClick = () => {
+    if (!isExpanded) {
+      setIsExpanded(true);
+      fetch("https://hacker-news.firebaseio.com/v0/user/" + story?.by + ".json")
+        .then((response) => response.json())
+        .then((data) => setAuthor(data));
+    }
+  };
+
+  return (
+    <div onClick={handleClick}>
+      {!isExpanded ? (
+        <div>
+          <h1>Title: {story?.title}</h1>
+          <p>Url: {story?.url}</p>
+          <p>Author: {story?.by}</p>
+          <p>Score: {story?.score}</p>
+        </div>
+      ) : (
+        <div>
+          <h1>Title: {story?.title}</h1>
+          <div> {story?.text} </div>
+          <a href={story?.url}>Url: {story?.url}</a>
+          <p>Author: {story?.by}</p>
+          <p>Score: {story?.score}</p>
+          <p>Karma: {!author?.karma ? "Loading" : author?.karma}</p>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default NewsItem;
--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,9 +1,24 @@
 import { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
 
 const NewsList = () => {
-  useEffect(() => {}, []);
+  const [data, setData] = useState<any[]>([]);
 
-  return <p>news list</p>;
+  useEffect(() => {
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+      .then((response) => response.json())
+      .then((data) => setData(data.splice(0, 20)));
+  }, []);
+
+  const listItems = data.map((item) => (
+    <NewsItem id={item} key={item}></NewsItem>
+  ));
+
+  return (
+    <div>
+      <ul>{listItems}</ul>
+    </div>
+  );
 };
 
 export default NewsList;
